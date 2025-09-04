f49edfcb9afde7da9a59db8f153c42f97dddea1d
brady-etzkorn <bre30104@gmail.com>
2025-09-04 11:35:33 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,10 @@
+#doing python.org assignment 
+
+'''
+After looking through the python website it is broken up into different sections.
+The main section that had stuck out to me was the section about success stories, it was strictly just a folder about how python has been successful in different fields.
+What really stuck out to me from this section was the software development part, since it is something I'm looking to go get a career in. 
+Once clicking on thr software development page, a health care story jumped out at me. It had talked about the story of the company and what their goals were, and then what they had used python for. 
+In the end they used python to make an automated appointment scheduling and requestin server as well as online consultations. 
+This seemed to me like a huge deal for the world of coding and health care, and makes me excited for possible things that I could do with coding in my future. 
+'''
