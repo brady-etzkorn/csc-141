5b773bad95a170bebbeb966cfa639dce9103a21a
brady-etzkorn <bre30104@gmail.com>
2025-09-04 11:16:25 -0400

    Delete assignments/01_getting_started/01_01_python_org.py

--- assignments/01_getting_started/01_01_python_org.py
+++
@@ -1,7 +0,0 @@
-#doing python.org assignment 
-#After looking through the python website it is broken up into different sections.
-#There were sections for python news and python events. But the ones that had stuck out to me the most were the tabs that showed everything python is used for.
-#It goes on to talk about how python is used for things like website developing, software development, education, and scientific matter. 
-#The other section that had stuck out to me was the section talking about success stories, this section when hovering over it would give you a drop down menu with all the categories that python has been successful in. 
-#It also has a section where it can show you jobs that you can get for knowing how to use python, and get work for what you are able to do.
-#Overall the python website gives you a lot more insight into what python can do for you if you stick with it and become good at coding. It shows you possible futures for yourself. 
