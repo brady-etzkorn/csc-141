bb649288afcb16ef37afa982840612d64fb9d235
brady-etzkorn <bre30104@gmail.com>
2025-09-04 11:36:10 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,7 @@
+'''
+If I had infinite skills:
+I would like the ability to make games that people love and would want to become a big name in the gaming community.
+While working up to that I would like to be able to make good enough code to make myself a living that I enjoy and love.
+I would want to be able to give back to the gaming community by being able to code some amazing games that are interesting for the players. 
+
+'''
