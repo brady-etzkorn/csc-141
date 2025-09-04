555c6e7930f37aeec5b10a446ee1e48c1f5e8110
brady-etzkorn <bre30104@gmail.com>
2025-09-04 11:09:29 -0400

    Delete assignments/01_getting_started/01_03_infinite_skills.py

--- assignments/01_getting_started/01_03_infinite_skills.py
+++
@@ -1,4 +0,0 @@
-#Reflecting back to other programming experiences that I have had I think they will help me get by in this class.
-#I had taken an online computer science course in high school and had learned all the basics of coding, like how to print and use variables.
-#I think that knowing this will help me move quickly through this course and help me learn a deeper understanding of coding.
-#As well as eventually help me get to what I want to do with my coding/programming ability and be able to program games and get a career in this. 
