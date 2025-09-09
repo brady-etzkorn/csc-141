b020afb69394a7aaaebd9dd2d9ece4a5dc31c88b
brady-etzkorn <bre30104@gmail.com>
2025-09-09 10:01:14 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,9 @@
+#This program is using a variable to print my "favorite" number
+eight = 8 
+print (eight,'is great')
+
+#This code was used to show different ways to print the number 8 using the different symbols of math
+print(5+3)
+print(10-2)
+print(16/2)
+print(4*2)
