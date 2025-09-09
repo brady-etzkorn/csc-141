1bfa8dedcd0a17816b47f13dcd473d7f2f85bfe4
brady-etzkorn <bre30104@gmail.com>
2025-09-09 11:27:38 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_12_zen_of_python.py
@@ -0,0 +1,4 @@
+import this
+
+''' Reading through the Zen of Python it is easy to see that it is important that the code is better when it is understandable then if it were more complicated. '''
+''' As well as it tells you what you should be looking for in your code and what you should be going away from to make your code better as time goes on.'''
