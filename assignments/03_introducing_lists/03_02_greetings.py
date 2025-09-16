d0b482c434185762854bbdc014da8d4cb60ccdb9
brady-etzkorn <bre30104@gmail.com>
2025-09-16 11:13:36 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_02_greetings.py
@@ -0,0 +1,10 @@
+names = ['Patrick', 'Mason', 'Austin', 'Ang']
+message = f'Lets go to the movies {names[0].title()}.'
+message1 = f'Lets go to the movies {names[1].title()}.'
+message2 = f'Lets go to the movies {names[2].title()}.'
+message3 = f'Lets go to the movies {names[3].title()}.'
+
+print (message)
+print(message1)
+print (message2)
+print(message3)
