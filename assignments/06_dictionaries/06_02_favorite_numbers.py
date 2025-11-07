90696a690631a6cfc60b9176b78f28536219e6db
brady-etzkorn <bre30104@gmail.com>
2025-11-07 12:00:02 -0500

    Add files via upload

---
+++ assignments/06_dictionaries/06_02_favorite_numbers.py
@@ -0,0 +1,8 @@
+favorite_numbers = {'Patrick' : 35, 'Mason' : 25, 'Ang' : 6, 'Monica' : 67, 'Kate' : 5}
+
+print(f"Patrick's favorite number is {favorite_numbers['Patrick']}")
+print(f"Mason's favorite number is {favorite_numbers['Mason']}")
+print(f"Ang's favorite number is {favorite_numbers['Ang']}")
+print(f"Monica's favorite number is {favorite_numbers['Monica']}")
+print(f"Kate's favorite number is {favorite_numbers['Kate']}")
+
